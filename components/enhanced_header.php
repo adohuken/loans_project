28b68072cccaef57908133f316eea4795832c49e
adohuken <adohuken2005@gmail.com>
2026-04-23 09:56:43 -0600

    Add favicon and trigger deployment

--- components/enhanced_header.php
+++ components/enhanced_header.php
@@ -17,6 +17,7 @@ $logo_path = $logo_path ?? '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css?v=3.0">
     <link rel="stylesheet" href="assets/css/themes.css?v=1.0">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
